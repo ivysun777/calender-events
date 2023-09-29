ba23c30f05066a659e5b547b91379d59704d2697
ivysun777 <114521145+ivysun777@users.noreply.github.com>
2023-09-29 14:45:44 -0500

    plan router

--- pages/_app.js
+++ pages/_app.js
@@ -3,4 +3,4 @@ import '../styles/globals.css'
 function MyApp({Component, pageProps}) {
     return<Component {...pageProps} />
 }
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,6 @@ function HomePage(){
     return(
         <div>
             <h1>The Home page</h1>
-            <ul>
-                <li>
-                    <Link href="/portfolio"> Portfolio</Link>
-                </li>
-                <li>
-                    <link href="/clients">Clients</link>
-                </li>
-            </ul>
         </div>
     );
 }
